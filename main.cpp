a7a2bcb81350c16f4aa7aaccba0246f113d5c4e6
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2018-06-14 10:38:39 -0300

    Editar arquivo
    Editar arquivo 'main.cpp'.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <map>
 
 using namespace cv;
 using namespace std;
 
 Vec3b darkBlue(255, 0, 0);
 Vec3b blue(255, 255, 0);
-Vec3b black(159, 159, 159);
+Vec3b black(0, 0, 0);
 Vec3b green(0, 255, 0);
 Vec3b yellow(0, 255, 255);
 Vec3b red(0, 0, 255);
@@ -27,21 +28,40 @@ void filtroRGB(Mat img, uchar r, uchar g, uchar b) {
 		}
 }
 
-void f(Mat img) {
+Point getTarget(Mat img) {
 	Mat gray;
 	cvtColor(img, gray, CV_BGR2GRAY);
 	GaussianBlur(gray, gray, Size(9, 9), 2, 2);
 	vector<Vec3f> circles;
 	HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 1, gray.rows / 8, 200, 100, 0, 0);
-	for (int i = 0; i < circles.size(); i++)
-		cout << "x=" << circles[i][0] << ", y=" << circles[i][1] << ", radius=" << circles[1][2] << endl;
+	Point target(circles[0][0], circles[0][1]);
+	return target;
 }
 
-void getTargets(int order[]) {
-	for (int i = 0; i < 
+void getTargets(int order[], int numTargets) {
+	Mat img, kernel;
+
+	img = imread("targets.png");
+	kernel = Mat::ones(9, 9, CV_64F) / 25;
+
+	int i, j;
+	for (i = 0; i < numTargets; i++) {
+		Vec3b color = colors[order[i]];
+		Mat aux = imread("targets.png");
+		filtroRGB(aux, color[2], color[1], color[0]);
+
+		for (j = 0; j < 10; j++)
+			filter2D(aux, aux, -1, kernel);
+
+		Point target = getTarget(aux);
+		cout << "x=" << target.x << ", y=" << target.y << endl;
+	}
 }
 
 int main() {
+	int order[] = {0, 1, 2, 3, 4, 5, 6};
+	getTargets(order, 7);
+	/*
 	Mat img, aux, kernel;
 	img = imread("targets.png");
 
@@ -57,5 +77,6 @@ int main() {
 	namedWindow("img", WINDOW_NORMAL);
 	imshow("img", aux);
 	waitKey(0);
+	*/
 	return 0;
 }
